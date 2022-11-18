450623a107fa2ee95400e8bc41fcf2c2ded2041f
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-18 16:16:38 +0100

    Impostazione domande js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+let domandauno = prompt('Inserisci il tuo nome');
+let domandadue = prompt('inserisci il tuo cognome');
+let domandatre = prompt('Scrivi il tuo colore preferito');
+
+console.log(domandauno);
+console.log(domandadue);
+console.log(domandatre);
+
+
+
+
+
+
