697e0bf0c5e3bc8b7d7c9d901d1e09e8243325c5
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-18 16:52:34 +0100

    Inserimento domande + display +22

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
-let domandauno = prompt('Inserisci il tuo nome');
-let domandadue = prompt('inserisci il tuo cognome');
-let domandatre = prompt('Scrivi il tuo colore preferito');
+let firstName = prompt('Inserisci il tuo nome');
+let lastName = prompt('inserisci il tuo cognome');
+let favColor = prompt('Scrivi il tuo colore preferito');
 
-console.log(domandauno);
-console.log(domandadue);
-console.log(domandatre);
+console.log(firstName);
+console.log(lastName);
+console.log(favColor);
 
-document.getElementById('output').innerHTML = "nomecognomecolorepreferito22"
+document.getElementById('output').innerHTML = firstName + lastName + '22';
 
 
 
